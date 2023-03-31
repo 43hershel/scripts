68f6986acd9bf3efa9170106411d6b3cd4e4046e
43hershel <xcaze8@gmail.com>
2023-04-01 00:45:09 +0200

    .\fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -36,8 +36,6 @@ cd
 git clone https://github.com/43hershel/dotfiles
 
 echo "Setting up config files"
-#Installing "Oh my Zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #Move i3 dotfiles to .config directory 
 cd ~/.config/
@@ -47,17 +45,20 @@ sudo mv config config.save i3status.conf ~/.config/i3
 
 #Move kitty config to .config directory
 cd ~/.config/kitty
-rm kitty.conf
+rm kitty.conf 
 cd ~/dotfiles/kitty/
 unzip kitty-themes.zip -d ~/.config/kitty 
 sudo mv kitty.conf theme.conf ~/.config/kitty
 
 #Installing the fonts 
-cd ~/dotfiles
+cd ~/dotfiles 
 unzip JetBrainsMono-2.304.zip
-mv JetBrainsMono-2.304 /usr/share/fonts
+sudo mv fonts /usr/share/fonts
 
 #Move wallpaper to images directory 
 cd ~/dotfiles
 mv pexels-eberhard-grossgasteiger-691668.jpg ~/Imágenes/
+
+#Adding "Oh my Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
